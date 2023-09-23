9105ab3ee704d53e567b2af4720946e1bbc65ebe
frank-mck <fmckenna56330@gmail.com>
2023-09-23 19:18:07 +0100

    chore: add form validation

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -1,5 +1,35 @@
-const formValidation = () => {
-  return;
-}
+import { z } from "zod";
 
-export default formValidation
+const mediaTypeValues = ["audio", "video", "image"] as const;
+
+const validation = z.object({
+  keywords: z
+    .string()
+    .min(2, { message: "Keywords must have at least 2 characters." })
+    .max(50, { message: "Keywords must have at most 50 characters." })
+    .nonempty({ message: "Keywords are required" })
+    .default(""),
+
+  mediaType: z
+    .union([
+      z.enum(mediaTypeValues, {
+        required_error: "Please select a valid media type",
+      }),
+      z.literal(null),
+      z.literal(undefined),
+    ])
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Please select a media type",
+    }),
+
+  yearStart: z
+    .string()
+    .regex(/^[0-9]+$/, { message: "Please enter a valid number." })
+    .min(1900, { message: "Year start must be after 1900." })
+    .max(new Date().getFullYear(), {
+      message: "Year start must not be in the future.",
+    })
+    .default(""),
+});
+
+export default validation;
