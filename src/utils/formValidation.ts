aa9754e482f5b35dbef4f0b468dbb34a1c4586d2
frank-mck <fmckenna56330@gmail.com>
2023-09-24 10:34:44 +0100

    chore: create validate function

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -1,35 +1,45 @@
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 const mediaTypeValues = ["audio", "video", "image"] as const;
 
 const validation = z.object({
   keywords: z
-    .string()
+    .string().trim()
     .min(2, { message: "Keywords must have at least 2 characters." })
     .max(50, { message: "Keywords must have at most 50 characters." })
-    .nonempty({ message: "Keywords are required" })
+    .nonempty({ message: "Keywords are required." })
     .default(""),
 
-  mediaType: z
-    .union([
-      z.enum(mediaTypeValues, {
-        required_error: "Please select a valid media type",
-      }),
-      z.literal(null),
-      z.literal(undefined),
-    ])
-    .refine((value) => value !== null && value !== undefined, {
-      message: "Please select a media type",
-    }),
+  mediaType: 
+    z.enum(mediaTypeValues, {
+      required_error: "Please select a media type.",
+    })
+    .optional()
+    .nullable(),
 
   yearStart: z
-    .string()
+    .string().trim()
+    .length(4)
     .regex(/^[0-9]+$/, { message: "Please enter a valid number." })
     .min(1900, { message: "Year start must be after 1900." })
     .max(new Date().getFullYear(), {
       message: "Year start must not be in the future.",
     })
-    .default(""),
+    .default("")
+    .nullable()
+    .optional(),
 });
 
-export default validation;
+const validate = (data: FormData): string[] | null => {
+  try {
+    validation.parse(data);
+    return null;
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return error.issues.map((issue) => issue.message);
+    }
+    throw error;
+  }
+};
+
+export default { validation, validate };
